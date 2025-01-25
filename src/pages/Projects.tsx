833fac7bbf6825f2454bbe0b20ef65868af4f7cf
walidez123 <62304458+walidez123@users.noreply.github.com>
2025-01-25 18:24:41 +0200

    updated links

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -27,7 +27,7 @@ const projects = {
     features: [
       "Live tracking with web socket in nodejs",
     ],
-    github: "",
+    github: "https://github.com/walidez123/live-tracking",
     live: "not available live for now"
   },
   // 'chat-app': {
