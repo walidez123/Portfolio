0f8749b1de46f9d97e29c22af7afac47b79b25bf
walidez123 <62304458+walidez123@users.noreply.github.com>
2025-01-26 16:48:42 +0200

    update skills 2

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,8 +3,8 @@ import { Code2, Globe, Laptop } from 'lucide-react';
 
 export default function About() {
   const skills = [
-    { name: 'Frontend', items: ['React', 'TypeScript', 'Tailwind CSS', 'Next.js', 'Vue'] },
-    { name: 'Backend', items: ['Node.js', 'PHP', 'Laravel', 'Express', 'MongoDB', 'PostgreSQL'] },
+    { name: 'Frontend', items: ['React', 'TypeScript', 'Tailwind CSS', 'Next.js', 'Vue', "Redux","Zustand" ] },
+    { name: 'Backend', items: ['Node.js', 'PHP', 'Laravel', 'Express', 'MongoDB', 'PostgreSQL',"MySQL" ,"Redis"] },
     { name: 'Tools', items: [ "Web Socket",'Git', 'Docker', 'Figma', 'APIs for mobile applications','Firebase', 'Postman'] },
     { name: 'Other', items: ['AWS', 'CI/CD', 'Testing', 'Agile'] },
   ];
